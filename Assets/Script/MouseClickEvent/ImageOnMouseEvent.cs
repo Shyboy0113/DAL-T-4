70a04e8963fa5ac1975380559b20c70bbc075227
shyboy0113 <shyboy0113@gmail.com>
2025-07-28 18:00:55 +0900

    1차 폴더 정리
    불필요한 폴더들 정리

--- Assets/Script/MouseClickEvent/ImageOnMouseEvent.cs
+++ Assets/Script/MouseClickEvent/ImageOnMouseEvent.cs
@@ -4,28 +4,29 @@ using UnityEngine.EventSystems;
 
 public class ImageOnMouseEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Color hoverColor = Color.yellow;
+    [SerializeField] private float hoverScale = 1.1f;
+    
     private Image _image;
-    private RectTransform _imageRect;
     private Vector2 _originalSize;
     private Color _originalColor;
 
     private void Start()
     {
         _image = GetComponent<Image>();
-        _imageRect = _image.GetComponent<RectTransform>();
-        _originalSize = _imageRect.sizeDelta;  // 원래 크기 저장
+        _originalSize = _image.rectTransform.sizeDelta;  // 원래 크기 저장
         _originalColor = _image.color;  // 원래 색상 저장
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        _imageRect.sizeDelta = _originalSize * 1.1f;  // 이미지 크기만 1.1배 증가
+        _image.rectTransform.sizeDelta = _originalSize * 1.1f;  // 이미지 크기만 1.1배 증가
         _image.color = Color.yellow;  // 색상을 노란색으로 변경
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        _imageRect.sizeDelta = _originalSize;  // 원래 크기로 복귀
+        _image.rectTransform.sizeDelta = _originalSize;  // 원래 크기로 복귀
         _image.color = _originalColor;  // 원래 색상으로 복귀
     }
 }
