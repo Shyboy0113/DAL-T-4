efe9942fdbfb8c589f0cf8ba661e5ac7c79f5250
shyboy0113 <shyboy0113@gmail.com>
2025-08-17 16:07:47 +0900

    해상도 코드 리팩토링
    기존에 갖고 있던 몇 가지 불필요한 점들을 개선했다.
    
    1.RefreshRate를 RefreshRateRatio로 변경
    2. FullScreen String을 PlayerPrefs 코드 내부가 아닌, const string으로 변경해서 작성
    3. 동일한 해상도지만, 주사율(Hz)만 다른 경우 최고 주사율만 반영한 뒤 나머지 중복 해상도 값들은 전부 제거했다.

--- Assets/02.Scripts/Option(UI)/ResolutionUI.cs
+++ Assets/02.Scripts/Option(UI)/ResolutionUI.cs
@@ -1,49 +1,84 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq; // Sort를 위해 추가될 수 있으나, List.Sort는 기본 포함
 using UnityEngine;
-using UnityEngine.UI; // DropDown을 사용하기 위한 기능
-
-using TMPro; // TextMeshPro
+using UnityEngine.UI;
+using TMPro;
 
 public class ResolutionUI : MonoBehaviour
 {
-    [SerializeField]
-    private Resolution[] resolutions;
+    // PlayerPrefs 키를 상수로 관리하여 오타 방지
+    private const string FullScreenPrefKey = "FullScreen";
+
+    public Resolution[] resolutions;
 
     public TMP_Dropdown resolutionDropdown;
     public Toggle fullscreenToggle;
 
-    private bool isOk = false;
-    
     private Resolution _selectedResolution;
     private Resolution _originalResolution;
-
+    private bool _isChangeApplied = false;
+    
+    private CutoutFade _cutoutFade;
+    
     private void OnEnable()
     {
         _originalResolution = Screen.currentResolution;
     }
 
+    private void Awake()
+    {
+        // GetComponentInChildren 또는 다른 안전한 방식으로 찾는 것을 고려해볼 수 있습니다.
+        _cutoutFade = FindObjectOfType<CutoutFade>(); 
+    }
+
     void Start()
     {
-        bool isFullScreen = (PlayerPrefs.GetInt("FullScreen", 1) == 1);
-        Screen.fullScreen = isFullScreen;
-
+        InitializeFullscreen();
+        InitializeResolutions();
+    }
+    
+    // 전체 화면 관련 초기화를 담당하는 메서드
+    private void InitializeFullscreen()
+    {
+        bool isFullScreen = (PlayerPrefs.GetInt(FullScreenPrefKey, 1) == 1);
         fullscreenToggle.isOn = isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
 
-        resolutions = Screen.resolutions;
+    // 해상도 관련 초기화를 담당하는 메서드
+    private void InitializeResolutions()
+    {
+        // 1. 중복 제거 및 최고 주사율 필터링
+        Dictionary<string, Resolution> uniqueResolutions = new Dictionary<string, Resolution>();
+        foreach (Resolution res in Screen.resolutions)
+        {
+            string key = res.width + "x" + res.height;
+            if (!uniqueResolutions.ContainsKey(key) || res.refreshRateRatio.value > uniqueResolutions[key].refreshRateRatio.value)
+            {
+                uniqueResolutions[key] = res;
+            }
+        }
+        
+        // 2. 리스트로 변환 후 정렬
+        List<Resolution> filteredResolutions = new List<Resolution>(uniqueResolutions.Values);
+        filteredResolutions.Sort((a, b) => {
+            if (a.width != b.width) return a.width.CompareTo(b.width);
+            return a.height.CompareTo(b.height);
+        });
+        resolutions = filteredResolutions.ToArray();
+
+        // 3. 드롭다운 채우기
         resolutionDropdown.ClearOptions();
-
         int currentResolutionIndex = 0;
-
         List<string> options = new List<string>();
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+            // [수정됨] .refreshRateRatio.value를 사용하고 정수로 반올림
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " +
+                            Mathf.RoundToInt((float)resolutions[i].refreshRateRatio.value) + "hz";
             options.Add(option);
 
-            // Resolution.Equals()를 사용하여 가로, 세로, 주사율까지 정확하게 비교
             if (resolutions[i].Equals(Screen.currentResolution))
             {
                 currentResolutionIndex = i;
@@ -52,37 +87,34 @@ public class ResolutionUI : MonoBehaviour
 
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue(); // 새로 고침
+        resolutionDropdown.RefreshShownValue();
 
         _selectedResolution = Screen.currentResolution;
     }
 
     public void SetResolution(int resolutionIndex)
     {
-        isOk = false;
+        _isChangeApplied = false;
         _selectedResolution = resolutions[resolutionIndex];
     }
 
-    public void SetOk()
+    public void ApplyChange()
     {
-        isOk = true;
+        _isChangeApplied = true;
         Screen.SetResolution(_selectedResolution.width, _selectedResolution.height, Screen.fullScreen);
         
-        CutoutFade fade = FindObjectOfType<CutoutFade>().GetComponent<CutoutFade>();
-        if (fade is not null) fade.ResizeResolution();
-
+        if (_cutoutFade != null) _cutoutFade.ResizeResolution();
     }
 
     public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
-        PlayerPrefs.SetInt("FullScreen", isFullScreen ? 1 : 0);
+        PlayerPrefs.SetInt(FullScreenPrefKey, isFullScreen ? 1 : 0);
     }
 
     public void Return()
     {
-        if (isOk && _originalResolution.width != Screen.currentResolution.width &&
-            _originalResolution.height != Screen.currentResolution.height)
+        if (_isChangeApplied && !_originalResolution.Equals(Screen.currentResolution))
         {
             Screen.SetResolution(_originalResolution.width, _originalResolution.height, Screen.fullScreen);
         }
