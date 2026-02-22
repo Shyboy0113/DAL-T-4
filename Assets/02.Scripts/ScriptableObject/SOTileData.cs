b8d38cccbc801d4ecddff151f981ed60c984714d
Hong Kitae <shyboy0113@gmail.com>
2026-02-22 15:27:57 +0900

    feature : 스크립터블 오브젝트 에셋 구현

--- Assets/02.Scripts/ScriptableObject/SOTileData.cs
+++ Assets/02.Scripts/ScriptableObject/SOTileData.cs
@@ -16,4 +16,8 @@ public class SOTileData : ScriptableObject
     public int baseToggleActivationCount = 2;
     public TileColor baseColor = TileColor.White;
 
+    [Header("Teleport Settings")]
+    public int baseTeleportID = 0; // ID가 같은 Start/End Teleport 타일끼리 연결
+    
+
 }
