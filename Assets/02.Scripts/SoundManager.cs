84487dad241b9c1a394de6537a8510e03859fd64
Hong Kitae <shyboy0113@gmail.com>
2026-03-09 11:10:44 +0900

    fix : vfx 삭제 및 Stage Scene으로 비교하는 기능 삭제

--- Assets/02.Scripts/SoundManager.cs
+++ Assets/02.Scripts/SoundManager.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SoundManager : MonoBehaviour
 {
@@ -11,9 +10,9 @@ public class SoundManager : MonoBehaviour
     private AudioSource audioSource;
 
     public List<AudioClip> bgmList;
-    public List<AudioClip> vfxList;
 
-    [SerializeField] private string currentStage = "";
+    [SerializeField] private string currentStage;
+    [SerializeField] private AudioClip currentAudioClip;
     
     private void Awake()
     {
@@ -32,24 +31,33 @@ public class SoundManager : MonoBehaviour
 
     private void Start()
     {
+        currentAudioClip = bgmList[0];
+        
         audioSource.clip = bgmList[0]; //맨 첫번째 BGM을 기준으로 설정
         audioSource.Play();
     }
 
-    public bool CheckCurrentStage(string stageName)
-    {
-        //현재 스테이지와 새로 입력받는 스테이지가 같은지 비교
-        return currentStage.Equals(stageName);
-    }
-
     public void RenewalBGM(AudioClip audioClip, string stageName)
     {
-        if (!CheckCurrentStage(stageName))
+        if (audioClip == null) return;
+
+        if (audioSource.clip != null && audioSource.clip == audioClip)
         {
             currentStage = stageName; //stageName 갱신
-            
-            audioSource.clip = audioClip; // 각 스테이지에 배치한 BGM을 SoundManager에 적용
-            audioSource.Play(); // 새로운 브금으로 갱신
+            return;
         }
+        
+        currentStage = stageName;
+        audioSource.clip = currentAudioClip = audioClip;
+        
+        // 각 스테이지에 배치한 BGM을 SoundManager에 적용
+        // DOTween을 이용한 볼륨 페이드 아웃 -> 교체 -> 페이드 인
+        Sequence s = DOTween.Sequence();
+        s.Append(audioSource.DOFade(0, 0.5f).OnComplete(() => {
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }));
+        s.Append(audioSource.DOFade(1.0f, 0.5f)); // 새로운 브금으로 갱신
+
     }
 }
