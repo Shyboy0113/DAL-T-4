8f18275f8f5e8fcda3d3b78b0961447b01d6015c
Hong Kitae <shyboy0113@gmail.com>
2026-03-09 11:11:56 +0900

    featrue : Undo/Redo시, UI 창에 보이는 시퀀스 갱신하는 스크립트 구현

---
+++ Assets/02.Scripts/UndoRedoButtonUI.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UndoRedoButtonUI : MonoBehaviour
+{
+    public enum ButtonType { Undo, Redo }
+    [SerializeField] private ButtonType type;
+
+    private Button _button;
+    private TextMeshProUGUI _text; // 혹은 이미지 사용 시 Image
+    private CanvasGroup _canvasGroup; // 투명도 조절용 (권장)
+
+    [SerializeField] private Color activeColor = new Color(255f/255f, 255f/255f, 255f/255f, 196f/255f);
+    [SerializeField] private Color deActiveColor = new Color (0.75f, 0.75f, 0.75f, 196f/255f);
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        if (_canvasGroup == null) _canvasGroup = gameObject.AddComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        StackManager sm = FindObjectOfType<StackManager>();
+        if (sm != null)
+        {
+            UpdateButtonState(sm.UndoCount, sm.RedoCount);
+        }
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.UndoRedoCountChanged += UpdateButtonState;
+        
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.UndoRedoCountChanged -= UpdateButtonState;
+    }
+
+    private void UpdateButtonState(int undoCount, int redoCount)
+    {
+        int currentCount = (type == ButtonType.Undo) ? undoCount : redoCount;
+        bool isActive = currentCount > 0;
+
+        // 1. 버튼 클릭 기능 활성/비활성화
+        _button.interactable = isActive;
+
+        // 2. 시각적 연출 (투명도 및 색상)
+        if (isActive)
+        {
+            _canvasGroup.alpha = 1f;
+            if (_text != null) _text.color = activeColor; // 원래 색
+        }
+        else
+        {
+            _canvasGroup.alpha = 0.75f; // 반투명
+            if (_text != null) _text.color = deActiveColor; // 회색
+        }
+    }
+}
