04bb89d00f610ff741d193147a55275bf74418f6
Hong Kitae <shyboy0113@gmail.com>
2026-03-24 21:30:38 +0900

    refactor : EventSystem 관련 GameObject 추가
    1. Option Multi Scene에서 다시 Intro의 MainMenu로 넘어갔을 때 target을 유지해주는 기능 추가
    2. Option이 UI Panel에서 Multi Scene으로 바뀌면서, OptionPanel을 할당할 필요가 없어져 GameObject 삭제
    3. SceneManager.LoadScene -> SceneGroupLoader.LoadGroup으로 기능 변경

--- Assets/02.Scripts/Intro/Intro_MainMenuCanvas.cs
+++ Assets/02.Scripts/Intro/Intro_MainMenuCanvas.cs
@@ -1,22 +1,56 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class Intro_MainMenuCanvas : MonoBehaviour
 {
-    [SerializeField] private GameObject optionPanel;
     [SerializeField] private CutoutFade cutoutFade;
+
+    [SerializeField] private SO_SceneGroup stageSelect;
+        
+    public SO_UIEvent optionEvent;
+    
+    private GameObject _lastSelectedGameObject;
+
+    private void OnEnable()
+    {
+        optionEvent.OnActiveToggle.AddListener(SetSelectedOfMainMenu);
+    }
+
+    private void OnDisable()
+    {
+        optionEvent.OnActiveToggle.RemoveListener(SetSelectedOfMainMenu);
+    }
+
+    private void OnDestroy()
+    {
+        if (EventSystem.current != null) EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    private void SetSelectedOfMainMenu(bool active)
+    {
+        if (!active)
+        {
+            if (_lastSelectedGameObject != null)
+            {
+                EventSystem.current.SetSelectedGameObject(_lastSelectedGameObject);
+            }
+        }
+    }
 
     public void StartButton() // 게임 시작 버튼 클릭 시 Stage 선택창으로 넘어감
     {
         cutoutFade.FadeOut(() => 
         {
-            SceneManager.LoadScene("StageSelect");
+            SceneGroupLoader.LoadGroup(stageSelect);
         });
         
     }
     public void OptionButton()
     {
-        optionPanel.SetActive(true);
+        _lastSelectedGameObject = EventSystem.current.currentSelectedGameObject;
+        optionEvent.Raise(true);
     }
     
     public void ExitButton() // 게임 종료 버튼 클릭 시 게임 종료
