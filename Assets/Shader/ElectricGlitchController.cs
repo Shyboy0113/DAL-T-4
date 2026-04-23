0699a741ddb2b64a9d6651f2e451d731551011bb
Hong Kitae <shyboy0113@gmail.com>
2026-04-23 16:41:01 +0900

    refactor : Shader Mode -> Canvas로 수정
    Raw Image -> Image로 변경

--- Assets/Shader/ElectricGlitchController.cs
+++ Assets/Shader/ElectricGlitchController.cs
@@ -1,17 +1,14 @@
-using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ElectricGlitchController : MonoBehaviour
 {
-    [SerializeField] private Material glitchMaterialAsset; // 원본 쉐이더 에셋
-    [SerializeField] private RawImage glitchOverlay;
+    [SerializeField] private Image glitchOverlay;
 
     [Header("감전 연출 설정")]
     [SerializeField] private float duration = 0.3f;
     [SerializeField] private float peakIntensity = 0.2f;
-
     
     private void OnEnable()
     {
